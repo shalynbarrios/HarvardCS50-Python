268e743e3dc5d299925df0f8f2db8c33bde5b544
Shalyn Barrios <165353062+shalynbarrios@users.noreply.github.com>
2024-12-14 01:53:46 -0600

    new lesson notes

--- lesson0_3.py
+++ lesson0_3.py
@@ -27,3 +27,50 @@ x = int(input("insert a num")) # inner function value becomes the argument to th
 y = int(input("what's the next"))
 
 print(x + y)
+
+# to floats..
+
+x = float(input("insert a num")) # inner function value becomes the argument to the outer function
+y = float(input("what's the next"))
+
+print(int(x + y))
+
+# there is a function called round
+
+# round(number[, ndigits]) these are the available parameters
+# only tkaes one number (no star)
+# [] square brackes mean something optional
+# if you wanted to round to the 100th place, that would be 2
+# 2 decimal places
+
+x = float(input("insert a num")) 
+y = float(input("what's the next"))
+
+print(round(x + y))
+
+# what about making 1000 to 1,000? the comma convention?
+
+x = float(input("insert a num")) 
+y = float(input("what's the next"))
+
+z = round(x+y)
+
+print(f"{z:,}")
+
+### here is more rounding practice
+
+x = float(input("insert a num")) 
+y = float(input("what's the next"))
+
+z = round(x/y, 2) # this second arument says "give us two decimal places"
+
+print(z)
+
+# we can rewrite the exact same code by
+
+x = float(input("insert a num")) 
+y = float(input("what's the next"))
+
+z = x/y
+
+print(f"{z:.2f}")
