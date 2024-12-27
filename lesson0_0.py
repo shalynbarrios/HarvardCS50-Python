bf48b2850fbb12fb6dd8bb5f9cff5d600580b33b
Shalyn Barrios <165353062+shalynbarrios@users.noreply.github.com>
2024-12-26 22:25:45 -0600

    Updated lesson0_0.py

--- lesson0_0.py
+++ lesson0_0.py
@@ -1,78 +1,100 @@
-print("hello there!")
-# functions can have side effects
-# if we had print("hello"
-# it's missing the parenthesis
-# very big deal to the computer!
+# --------
+# Introduction to Python
+# --------
+# Hi everyone!
+# This is a collection of files to help guide you through Harvard's CS50 Intro to Python course
+# I recommend following Professor Malan as you skim through this guide
+# I start numbering with zero-based lessons, which will make sense why I did this mini-easter-egg once you know what lists are.
+# For now, interpret "Lesson 0" notes as Lecture 1 guide.
 
-# so to run code in vscode you say
+# --------
+# Introduction to Printing
+# --------
+print("Hello there!")
+# Functions can have side effects.
+# For example, if we had print("Hello" (missing the closing parenthesis),
+# it would cause an error. This is a big deal to the computer!
+
+# --------
+# Running Python Code
+# --------
+# To run your code in VS Code, use the following command in the terminal:
 # python (filename).py
 
+# --------
+# Using Input to Get User Data
+# --------
 print("What's your name? ")
 
-input() # but input already has a built in
-# paraameter to have a prompt
-# input(prompt)
+input()  
+# The `input()` function already has a built-in parameter to include a prompt.
+# Syntax: input(prompt)
 
-# this prompt is a string and input can only accept a string arguments
+# The prompt must be a string because `input()` only accepts strings as arguments.
 
-# so we can say
-
+# Example with a prompt:
 input("What's your name? ")
 
-response = input("What's your name? ")
-# the variable response hold's the input to the prompt 'What's your name?'
+# Assigning the user's input to a variable:
+response = input("What's your name? ")  
+# The variable `response` holds the input provided by the user.
 
-# the equals sign assignment
+# --------
+# Printing with Variables
+# --------
+print("Hello", response)  
+# The `print()` function can handle multiple arguments.
+# It automatically adds a space between them.
 
-######
+print("Hello" + response)  
+# Using the `+` operator concatenates strings directly, without spaces.
+# Both "Hello" and `response` must be strings. If `response` isn't a string
+# (e.g., it's a number), you must convert it using `str()`.
 
-print("hello", response)
-
-# When you want to combine strings (or other values)
-#  with automatic spacing and support for non-string types 
-# (e.g., numbers or objects).
-
-print("hello" + response)
-
-# What it does: The + operator concatenates strings directly without adding spaces. 
-# Both "hello" and response must be strings, or you'll get an error (e.g., 
-# if response is an integer, you'll need to convert it to a string with str()).
-
-######
-
+# --------
+# Numbers and Operations
+# --------
 num = 4
 
-print(2, num) # this is two arguments in the function print
+print(2, num)  # Outputs: 2 4 (two arguments separated by a space).
+# This just lists the numbers.
 
-# just lists these numbers
+print(2 + num)  # Outputs: 6 (adds the numbers before printing).
+# This performs addition.
 
-print(2 + num) # this is adding the two and then printing result
-# this is one argument
-
-# actually adds
-
-###
-
-# print automatically creates a new line
-# you can override this behavior
-# docs.python.org is the official python documentation
-
-# print(*objects, sep =" ", end"\n", file=sys.stdout, flush = False)
-
-# the name of this function is print
-# everything inside of the function are the parameters
-
-# objects* means that the function can take as many arguments as you want
-
-# the next parameter is sep which is seperate
-# when you pass in multiple arguments to print, there will be a
-# " " a space, which is what something like print(2, num) does
-
-# another parameter is end = '\n'
-# \n means new line
-# so we can override this
+# --------
+# Customizing Print Behavior
+# --------
+# By default, `print()` automatically creates a new line after output.
+# You can override this behavior using the `end` parameter.
 
 name = input("What's your name? ")
 
-print("Hello, ", end = "")
-print(name)
+# Example: Printing without a newline
+print("Hello, ", end="")  # Overrides the default new line.
+print(name)  # Continues on the same line.
+
+# --------
+# Exploring the Print Function
+# --------
+# `print()` has several parameters:
+# print(*objects, sep=" ", end="\n", file=sys.stdout, flush=False)
+
+# - `*objects`: Accepts multiple arguments (e.g., print(2, num)).
+# - `sep`: Defines the separator between arguments (default: a space).
+# - `end`: Determines what happens after the output (default: new line `\n`).
+# - `file`: Specifies the output file (default: sys.stdout).
+# - `flush`: Forces immediate output (default: False).
+
+# For example, when passing multiple arguments to `print()`:
+# print(2, num) will separate the numbers with a space because `sep=" "` by default.
+
+# You can also override `end` to avoid the automatic new line.
+
+# --------
+# Reference
+# --------
+# For more info, check the official Python documentation:
+# This is VERY helpful!!
+# https://docs.python.org
+
